189b0724e5ef426de6a33aa056177f1f1b1b2fbc
designmaster0928 <norrisdavis0928@gmail.com>
2023-07-31 18:36:11 -0700

    first step

---
+++ song_maker_(000)/assistant/secret_key.py
@@ -0,0 +1,1 @@
+API_KEY = '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'
---
+++ song_maker_(000)/assistant/views.py
@@ -0,0 +1,104 @@
+# importing render and redirect
+from django.shortcuts import render, redirect
+# importing the openai API
+import openai
+# import the generated API key from the secret_key file
+from .secret_key import API_KEY
+# loading the API key from the secret_key file
+openai.api_key = API_KEY
+
+# this is the home view for handling home page logic
+def home(request):
+    my_value = 0
+    try:
+        # if the session does not have a messages key, create one
+        if 'messages' not in request.session:
+            request.session['messages'] = [
+                
+            ]
+        if request.method == 'POST' and my_value == 1:
+            prompt= mission  
+            temperature = float(request.POST.get('temperature', 0.1))
+            # append the prompt to the messages list
+            # set the session as modified
+            request.session['messages'].append({"role": "user", "content": prompt})
+            request.session.modified = True
+            # call the openai API
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=request.session['messages'],
+                temperature=temperature,
+                max_tokens=1000
+            )
+            # format the response
+            formatted_response = response['choices'][0]['message']['content']
+            # append the response to the messages list
+            request.session['messages'].append({"role": "assistant", "content": formatted_response})
+            request.session.modified = True
+            # redirect to the home page
+            context = {
+                'messages': request.session['messages'],
+                'prompt': '',
+                'author' : auth,
+                'bio' :bio,
+                'temperature': temperature,
+            }
+            return render(request, 'assistant/home.html', context)
+        if request.method == 'POST' and my_value == 0:
+            my_value = 1
+            # get the prompt from the form
+            str1= "Here is who I am.'"
+            auth= request.POST.get('author')
+            bio= request.POST.get('bio')
+            mission= request.POST.get('mission')
+            prompt= str1+auth+"."+bio+"'. "+mission        
+            # get the temperature from the form
+            temperature = float(request.POST.get('temperature', 0.1))
+            # append the prompt to the messages list
+            # set the session as modified
+            request.session['messages'].append({"role": "user", "content": prompt})
+            request.session.modified = True
+            # call the openai API
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=request.session['messages'],
+                temperature=temperature,
+                max_tokens=1000
+            )
+            # format the response
+            formatted_response = response['choices'][0]['message']['content']
+            # append the response to the messages list
+            request.session['messages'].append({"role": "assistant", "content": formatted_response})
+            request.session.modified = True
+            # redirect to the home page
+            context = {
+                'messages': request.session['messages'],
+                'prompt': '',
+                'author' : auth,
+                'bio' :bio,
+                'temperature': temperature,
+            }
+            return render(request, 'assistant/home.html', context)
+        else:
+            # if the request is not a POST request, render the home page
+            context = {
+                'messages': request.session['messages'],
+                'prompt': '',
+                'temperature': 0.1,
+            }
+            return render(request, 'assistant/home.html', context)
+    except Exception as e:
+        print(e)
+        # if there is an error, redirect to the error handler
+        return redirect('error_handler')
+
+
+def new_chat(request):
+    # clear the messages list
+    request.session.pop('messages', None)
+    return redirect('home')
+
+
+# this is the view for handling errors
+def error_handler(request):
+    return render(request, 'assistant/404.html')
